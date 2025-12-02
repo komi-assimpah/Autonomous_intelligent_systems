431aa0f487f18d4febafc33aa7c1b3f76ed259af
komi-assimpah <komi-jean-paul.assimpah@etu.univ-cotedazur.fr>
2025-12-02 21:09:54 +0100

    refactor: update navigation node to handle detection commands and streamline emergency stop

--- src/object_search_navigation/object_search_navigation/navigation_node.py
+++ src/object_search_navigation/object_search_navigation/navigation_node.py
@@ -1,6 +1,5 @@
 import rclpy
 from rclpy.node import Node
-from sensor_msgs.msg import Image
 from geometry_msgs.msg import Twist
 from std_msgs.msg import String
 
@@ -10,55 +9,44 @@ class NavigationNode(Node):
         super().__init__('navigation_node')
         self.get_logger().info('Navigation node started')
         
-        # Publisher pour republier les images de la caméra
-        self.camera_pub = self.create_publisher(
-            Image, 
-            '/search/camera_feed',
-            10
-        )
-        
         self.cmd_vel_pub = self.create_publisher(
             Twist,
             '/cmd_vel',
             10
         )
         
-        # Subscriber pour recevoir les images de la caméra
-        self.camera_sub = self.create_subscription(
-            Image,
-            '/camera/image_raw',
-            self.camera_callback,
+        self.detection_sub = self.create_subscription(
+            String,
+            '/detection/command',
+            self.detection_callback,
             10
         )
         
-        # Subscriber pour recevoir le statut du détecteur
-        self.status_sub = self.create_subscription(
-            String,
-            '/search/status',
-            self.status_callback,
-            10
-        )
+        self.get_logger().info('✅ Listening for detection on /detection/command')
+        #TODO: Integrate Nav2 for autonomous exploration, after understanding how it's currently done    
     
-    # Callback appelée quand une image arrive de la caméra
-    def camera_callback(self, msg):
-        """Republier l'image sur /search/camera_feed pour le détecteur"""
-        self.camera_pub.publish(msg)
     
-    # Callback appelée quand le statut change (SEARCHING ou STOP)
-    def status_callback(self, msg):
+    def detection_callback(self, msg):
         if msg.data == "STOP":
-            self.get_logger().info("Object found!!!, stopping")
-            # Twist() crée un message avec toutes les vitesses à 0
-            stop_msg = Twist()
-            self.cmd_vel_pub.publish(stop_msg)
+            self.get_logger().info('OBJECT FOUND! Stopping mission.')
+            self.emergency_stop()
+    
+    
+    def emergency_stop(self):
+        self.cmd_vel_pub.publish(Twist())
+        self.get_logger().info('Emergency stop executed - Mission complete!')
 
 
 def main(args=None):
     rclpy.init(args=args)
     node = NavigationNode()
-    rclpy.spin(node)
-    node.destroy_node()
-    rclpy.shutdown()
+    
+    try:
+        rclpy.spin(node)
+    finally:
+        node.emergency_stop()
+        node.destroy_node()
+        rclpy.shutdown()
 
 
 if __name__ == '__main__':
--- src/object_search_navigation/setup.py
+++ src/object_search_navigation/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'object_search_navigation'
 
@@ -10,6 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -25,6 +28,7 @@ setup(
     entry_points={
         'console_scripts': [
             'navigation_node = object_search_navigation.navigation_node:main',
+            'camera_processor_node = object_search_navigation.camera_processor_node:main',
         ],
     },
 )
